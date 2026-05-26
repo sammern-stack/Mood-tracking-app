d4736838f08477246e5556e34fe85461b3867366
sammern <samreactdev@gmail.com>
2026-05-26 23:24:31 +0300

    refactor(main): reorganize imports for better readability and add css reset styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "./styles/_reset.scss";
+import "./index.scss";
+
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
