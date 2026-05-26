0b0f62f70d174cb07c69c52a163d1e9c146359fa
sammern <samreactdev@gmail.com>
2026-05-26 21:55:57 +0300

    feat: create an AppLoader component for better UX

---
+++ client/src/components/common/AppLoader/AppLoader.tsx
@@ -0,0 +1,8 @@
+import './AppLoader.scss'
+
+export const AppLoader = () => {
+  return (
+    // TODO: Add a better styled loading page here
+    <div className='app-loader'>Loading...</div>
+  );
+};
